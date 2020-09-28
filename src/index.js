a4ea9cde321604e948fde4c73f405171d76c8863
unknown <KLIM_NIKITA_2001@mail.rugit config --global user.email KLIM_NIKITA_2001@mail.rugit config --global user.email KLIM_NIKITA_2001@mail.ru>
2020-09-28 19:09:20 +0300

    feat: done tasks

--- src/index.js
+++ src/index.js
@@ -1,12 +1,52 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (array === undefined) {
+    return 0;
+  } else if (array.length === 0) {
+    return 0;
+  } else {
+    let min = array[0];
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] < min) {
+        min = array[i];
+      }
+    }
+    return min;
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+
+exports.max = function max(array) {
+
+  if (array === undefined) {
+    return 0;
+  } else if (array.length === 0) {
+    return 0;
+  } else {
+    let max = array[0];
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] > max) {
+        max = array[i];
+      }
+    }
+    return max;
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
-}
+  exports.avg = function avg(array) {
+    if (array === undefined) {
+      return 0;
+    } else if (array.length === 0) {
+      return 0;
+    } else {
+      let sum = 0;
+      for (let i = 0; i < array.length; i++) {
+        sum = sum + array[i];
+      }
+      let avr = sum / array.length;
+      return avr;
+    }
+
+  }
+
+
